7a695883e5bf13a13099702390e745d2978fd25b
hugosenari <hugosenari@gmail.com>
2022-03-12 02:20:25 -0300

    chore: re index figures

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''unicode symbols'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."figures-master".url = "path:./master";
+  inputs."figures-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."figures-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."figures-v1_0_0".url = "path:./v1_0_0";
+  inputs."figures-v1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."figures-v1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."figures-v1_1_0".url = "path:./v1_1_0";
+  inputs."figures-v1_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."figures-v1_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."figures-v1_2_0".url = "path:./v1_2_0";
+  inputs."figures-v1_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."figures-v1_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''unicode symbols'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-figures-master.flake = false;
+  inputs.src-figures-master.owner = "lmariscal";
+  inputs.src-figures-master.ref   = "refs/heads/master";
+  inputs.src-figures-master.repo  = "figures";
+  inputs.src-figures-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-figures-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-figures-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
